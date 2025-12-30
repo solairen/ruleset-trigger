18134cd304b11749009b50ef2417ae4399677341
Michał Oleszek <moleszek@duck.com>
2025-12-30 08:37:28 +0100

    GH-8 [Task]: Add Pre-Commit Hooks, Markdownlint, and Secrets Detection (#23)

--- github_ruleset.py
+++ github_ruleset.py
@@ -1,34 +1,27 @@
 import os
+
 import requests
 
 github_token = os.environ.get("INPUT_TOKEN")
-repository = (
-  os.environ.get("INPUT_REPOSITORY")
-  or os.environ.get("GITHUB_REPOSITORY")
-)
+repository = os.environ.get("INPUT_REPOSITORY") or os.environ.get("GITHUB_REPOSITORY")
 ruleset_id = os.environ.get("INPUT_RULESET_ID")
-enforcement = (
-  os.environ.get("INPUT_ENFORCEMENT")
-  or "active"
-)
+enforcement = os.environ.get("INPUT_ENFORCEMENT") or "active"
 
 headers = {
     "Accept": "application/vnd.github.v3+json",
     "Authorization": f"Bearer {github_token}",
-    "X-GitHub-Api-Version": "2022-11-28"
+    "X-GitHub-Api-Version": "2022-11-28",
 }
 
-data = {
-    "enforcement": enforcement
-}
+data = {"enforcement": enforcement}
 
 url = f"https://api.github.com/repos/{repository}/rulesets/{ruleset_id}"
 
 response = requests.patch(url, headers=headers, json=data)
 
 if response.status_code == 200:
-  print(f"Status code: {response.status_code}")
+    print(f"Status code: {response.status_code}")
 else:
-  print(f"Error: {response.status_code}")
-  print(response.text)
-  exit(1)
+    print(f"Error: {response.status_code}")
+    print(response.text)
+    exit(1)
